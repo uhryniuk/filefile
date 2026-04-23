34f0c48741914c8ba7482baf486fdb3033bab108
uhryniuk <duhry99@gmail.com>
2026-04-23 09:26:54 -0500

    rewrite apply as recursive walk; honor file contents

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,14 @@
 use crate::{
-    common::{combine_path, validate_path},
+    common::{self, combine_path, validate_path},
     filefile, get_cwd,
     node::{self, Node},
 };
-use std::{fs::File, io::Read, io::Write, path::PathBuf};
+use std::{
+    fs::{self, File},
+    io::Read,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
 use anyhow::{self, Result};
 
@@ -52,6 +57,31 @@ impl<'a> ApplyCommand<'a> {
     }
 }
 
+fn apply_node(node: &Node, parent: &Path, dry: bool) -> Result<()> {
+    let path = parent.join(&node.basename);
+    match node.node_type {
+        node::NodeType::DIRECTORY => {
+            if dry {
+                eprintln!("DRY mkdir {:?}", path);
+            } else {
+                fs::create_dir_all(&path)?;
+            }
+            for child in &node.children {
+                apply_node(child, &path, dry)?;
+            }
+        }
+        node::NodeType::FILE => {
+            let body = node.contents.clone().unwrap_or_default();
+            if dry {
+                eprintln!("DRY write {:?} ({} bytes)", path, body.len());
+            } else {
+                fs::write(&path, body)?;
+            }
+        }
+    }
+    Ok(())
+}
+
 impl<'a> Command for ApplyCommand<'a> {
     fn execute(&self) -> Result<()> {
         let (path, input) = match self.parse_args() {
@@ -62,70 +92,19 @@ impl<'a> Command for ApplyCommand<'a> {
             }
         };
 
-        println!(
-            "Running apply command {:?}, {:?}",
-            path.clone(),
-            input.clone()
-        );
-
-        // read input file into string buffer
         let mut raw_yaml = String::new();
         let mut f = File::open(&input)?;
         f.read_to_string(&mut raw_yaml)?;
-
-        println!("{}", raw_yaml);
 
         let mut values: serde_yaml::Value =
             serde_yaml::from_str(&raw_yaml).expect("Couldn't convert yaml string to Value");
         let root_nodes = node::convert_value(&mut values);
 
-        let mut node_queue: Vec<Vec<Node>> = vec![root_nodes.clone()];
-        while let Some(mut nodes) = node_queue.pop() {
-            for node in nodes.iter_mut() {
-                // Update the directory to use provided path...
-                node.prefix_path(path.as_str());
-
-                // TODO for debugging only...
-                println!("{:?}", node);
-
-                match node.node_type {
-                    node::NodeType::FILE => {
-                        std::fs::write(node.path_as_str().as_str(), "")?;
-                    }
-                    node::NodeType::DIRECTORY => {
-                        std::fs::create_dir_all(node.path_as_str().as_str())?;
-                    }
-                }
-
-                // Push the children of the current node onto the queue
-                if !node.children.is_empty() {
-                    node_queue.push(node.children.clone());
-                }
-            }
+        let dry = common::get_global_state().dry_run();
+        let root = Path::new(&path);
+        for node in &root_nodes {
+            apply_node(node, root, dry)?;
         }
-
-        // 1. Convert to values
-        // 2. Convert to nodes.
-        // Tagged -> Operation
-        // 3. Iterate & Execute
-
-        // // Context is the the root directory to run the apply command in.
-        // let mut context = Node::new(&path);
-        // let mut nodes: Vec<Node> = node::parse_yaml(&data);
-        // context.add_children(nodes.clone());
-        //
-        // // node::print_graph(context, 0);
-        //
-        //
-        // // TODO at this point we have the file system...
-        // // Now what do we want to do this it?
-        // // DFS iterate over it, keeping anything besides NOOP nodes.
-        // // Then we call generate again, to create a new file...
-        //
-        // let ctx = &mut common::get_global_state();
-        // if !ctx.dry_run() {
-        //     println!("Running apply command");
-        // }
 
         Ok(())
     }
---
+++ tests/apply.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::process::Command;
+
+const BIN: &str = env!("CARGO_BIN_EXE_filefile");
+
+#[test]
+fn apply_creates_nested_dir_and_file_with_contents() {
+    let td = tempfile::tempdir().unwrap();
+    let root = td.path();
+    let filefile = root.join("Filefile.yaml");
+    fs::write(
+        &filefile,
+        "hello:\n  - world: \"contents of the file\"\n  - here:\n    - I: \"am\"\n",
+    )
+    .unwrap();
+
+    let status = Command::new(BIN)
+        .args(["apply", "-p", root.to_str().unwrap(), "-i", filefile.to_str().unwrap()])
+        .status()
+        .unwrap();
+    assert!(status.success(), "apply exited non-zero");
+
+    assert!(root.join("hello").is_dir());
+    assert_eq!(
+        fs::read_to_string(root.join("hello/world")).unwrap(),
+        "contents of the file"
+    );
+    assert!(root.join("hello/here").is_dir());
+    assert_eq!(fs::read_to_string(root.join("hello/here/I")).unwrap(), "am");
+}
